23dbdd5033f16f482159a7600ac218b8967cc3a6
Squitch <63391793+SquitchYT@users.noreply.github.com>
2021-02-12 15:01:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,98 @@
+const time1 = new Date().getTime()
+
+const { app, BrowserWindow, ipcMain : ipc} = require('electron')
+
+const electron = require("electron")
+const pty = require("node-pty");
+
+let mainWindow;
+let shells = []
+
+function openWindow() {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWindow.removeMenu()
+    mainWindow.loadFile("src/index.html")
+    mainWindow.on("closed", function() {
+        mainWindow = null;
+    });
+
+
+    ipc.on("load-end", (e, ea) => {
+        time2 = new Date().getTime()
+        time = time2 - time1
+        console.log("launch in :" + time + "ms")
+    })
+}
+
+
+ipc.on('new-term', (e, data) => {
+    let shell = pty.spawn("bash", [], {
+        name: "xterm-color",
+        cols : data.cols,
+        rows : data.rows,
+        cwd: process.env.HOME,
+        env: process.env,
+    })
+
+    shell.on('data', (datas) => {
+        mainWindow.webContents.send('pty-data', {
+            index : data.index,
+            data : datas
+        })
+    })
+
+    let s = {
+        index : data.index,
+        shell : shell
+    }
+
+    shells.push(s)
+})
+
+ipc.on('terminal-data', (e, data) => {
+    shells.forEach((el) => {
+        if (el.index == data.index) {
+            el.shell.write(data.data)
+        } 
+    })
+})
+
+
+// App events
+app.on("ready", openWindow);
+
+app.on("window-all-closed", function() {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", function() {
+    if (mainWindow == null) {
+        createWindow();
+    }
+});
+
+
+ipc.on("log", (e, data) => {
+    console.log(data)
+})
+
+
+ipc.on("close-terminal", (e, data) => {
+    let y = 0
+    shells.forEach((el) => {
+        if (el.index == data) {
+            shells.splice(y, 1)
+        }
+        y++;
+    })
+})
+
+ipc.on('close', (e,data) => {
+    app.quit()
+})
---
+++ src/app.js
@@ -0,0 +1,200 @@
+const { Terminal } = require('xterm')
+const ipc = require('electron').ipcRenderer
+const fs = require('fs')
+const os = require('os')
+
+const homedir = os.homedir()
+const tabs = document.querySelector('.tabs')
+const terminals = document.querySelector('.terminals')
+const viewport = document.getElementById("terminals")
+const body = document.getElementById('body')
+
+let terminalsList = []
+let n = 0
+
+let index = 0
+
+
+let colors = {
+    terminal : {
+        foreground : '#fff',
+        background : '#000'
+    },
+    app : {
+        tab_background : '#000'
+    }
+}
+
+getTheme('tokyo-night')
+
+function getTheme(theme) {
+    let file =  fs.readFileSync(homedir + '/.config/tess/theme/' + theme + '.theme', 'utf-8').split('\n').filter(Boolean)
+
+    file.forEach((line) => {
+        if (line.startsWith('foreground')) {
+            let prop = line.split(': ')
+            colors.terminal.foreground = prop[1]
+        } else if (line.startsWith('background')) {
+            let prop = line.split(': ')
+            colors.terminal.background = prop[1]
+        } else if (line.startsWith('app-background')) {
+            let prop = line.split(': ')
+            colors.app.tab_background = prop[1]
+        } else if (line.startsWith('app-foreground')) {
+            let prop = line.split(': ')
+            colors.app.tab_foreground = prop[1]
+        } else if (line.startsWith('app-color')) {
+            let prop = line.split(': ')
+            colors.app.text_color = prop[1]
+        }
+    })
+}
+
+tabs.style.background = colors.app.tab_background
+body.style.color = colors.app.text_color
+
+CreateNewTerminal()
+
+ipc.send('load-end')
+
+
+window.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.shiftKey && e.key === 'T'){
+        CreateNewTerminal()
+    }
+})
+
+ipc.on('pty-data', (e, data) => {
+    terminalsList.forEach((el) => {
+        if (el.index === data.index) {
+            el.term.write(data.data)
+        } 
+    })
+})
+
+
+function CreateNewTerminal() {
+    let tab = document.createElement('div')
+
+    tab.style.background = colors.app.tab_foreground
+
+    let tab_link = document.createElement('div')
+    tab_link.addEventListener('click', () => {
+        let terms = document.getElementsByClassName('terms')
+        let tabs = document.getElementsByClassName('tab')
+
+        console.log(terms)
+
+        let i = 0
+        while (i < terms.length) {
+            let a = terms.item(i)
+            let r = tabs.item(i)
+            a.classList.add('hidden')
+            a.classList.remove('visible')
+            r.style.background = colors.app.tab_background
+            i++        
+        }
+
+
+        tab.style.background = colors.app.tab_foreground
+
+        let termtoview = document.querySelector('.terminal-' + tab_link.classList[2])
+        termtoview.classList.remove('hidden')
+        termtoview.classList.add('visible')
+
+        n = termtoview.getAttribute('number')
+
+        terminalsList.forEach((el) => {
+            if (el.index == n) {
+                el.term.focus()
+            } 
+        })
+    })
+    tab_link.innerHTML = "Bash ~ $"
+    tab.classList.add('tab', 'tab-all-' + index)
+    tab_link.classList.add('tab-link', 'tab-' + index, index)
+
+    let close_button = document.createElement('div')
+    close_button.classList.add('close-button')
+    close_button.setAttribute('close-button-number', index)
+    close_button.innerHTML = "X"
+
+    close_button.addEventListener('click', () => {
+        o = close_button.getAttribute('close-button-number')
+
+        ipc.send('close-terminal', o)
+
+        let te = document.querySelector('.terminal-' + o)
+        let ta = document.querySelector('.tab-all-' + o)
+
+        ta.remove()
+        te.remove()
+
+        let y = 0
+
+        terminalsList.forEach((el) => {
+            if (el.index == o) {
+                terminalsList.splice(y, 1)
+            }
+            y++
+        })
+
+        if (terminalsList.length === 0) {
+            ipc.send('close')
+        }
+    })
+
+    tab.appendChild(tab_link)
+    tab.appendChild(close_button)
+    tabs.appendChild(tab)
+
+    let termDiv = document.createElement('div')
+    termDiv.classList.add('terms', 'terminal-' + index, 'visible')
+    termDiv.setAttribute('number', index)
+
+    let height = viewport.clientHeight
+    let width = viewport.clientWidth;
+    let rows = parseInt(height/16.95, 10);
+    let cols = parseInt(width/9, 10);
+
+    let term = new Terminal({
+        cols : cols,
+        rows : rows,
+        theme : colors.terminal
+    })
+    term.open(termDiv)
+
+    term.onData((e) => {
+        ipc.send('terminal-data', {
+            index : n,
+            data : e
+        })
+    })
+
+    let terms = document.getElementsByClassName('terms')
+
+    let i = 0
+    while (i < terms.length) {
+        let a = terms.item(i)
+        a.classList.add('hidden')
+        i++
+    }
+
+    ipc.send('new-term', {
+        index : index,
+        rows : rows,
+        cols : cols
+    })
+
+    n = index
+
+    let t = {
+        index : index,
+        term : term
+    }
+
+    index++;
+    
+    terminals.appendChild(termDiv)
+    terminalsList.push(t)
+}
