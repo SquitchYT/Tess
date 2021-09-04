cef8df091783e7d29bc87dcfc694ba0d50834347
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-09-04 20:27:10 +0200

    Handle CPR with xmake, try to see if it works

---
+++ cli/xmake.lua
@@ -0,0 +1,15 @@
+set_project("tess-cli")
+set_version("0.0.0")
+
+add_rules("mode.debug", "mode.release")
+set_languages("cxx17")
+
+add_requires("cpr 1.6.2")
+
+target("cli")
+    set_kind("binary")
+
+    add_files("**.cpp")
+    add_headerfiles("**.hpp")
+
+    add_packages("cpr")
