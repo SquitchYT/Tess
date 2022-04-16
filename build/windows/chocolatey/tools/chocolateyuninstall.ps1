f0f5de37f4ebd6ca54f435497c5ca370ddf3d5ef
Squitch <clementfoure2005@gmail.com>
2022-04-16 11:25:14 +0200

    Cleaning Chocolatey Package

--- build/windows/chocolatey/tools/chocolateyuninstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'tess*'
-  fileType      = 'exe'
-
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  validExitCodes= @(0)
-}
